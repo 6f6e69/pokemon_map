cad0b86eb3bc97d838e7e5ef53368580041317bb
alexander Mityukov <a.m.mityukov@gmail.com>
2023-07-09 21:52:21 +0200

    Remove unused type hinting, add verbose name to models and it's fields

--- src/pokemon_entities/models.py
+++ src/pokemon_entities/models.py
@@ -1,57 +1,77 @@
 from django.db import models  # noqa F401
 from django.utils.timezone import localtime
-from pytz import timezone
+from datetime import tzinfo
 
 
 class Pokemon(models.Model):
-    title: models.CharField = models.CharField(max_length=200)
-    title_en: models.CharField = models.CharField(max_length=200,
-                                                  blank=True,
-                                                  default="")
-    title_jp: models.CharField = models.CharField(max_length=200,
-                                                  blank=True,
-                                                  default="")
-    image: models.ImageField = models.ImageField(blank=True)
-    previous_evolution: models.ForeignKey = models.ForeignKey(
-                                                "self",
-                                                on_delete=models.SET_NULL,
-                                                blank=True,
-                                                null=True)
-    description: models.TextField = models.TextField(blank=True,
-                                                     default="")
+    title = models.CharField(max_length=200,
+                             verbose_name="название (русское)")
+    title_en = models.CharField(max_length=200,
+                                blank=True,
+                                default="",
+                                verbose_name="название (английское)")
+    title_jp = models.CharField(max_length=200,
+                                blank=True,
+                                default="",
+                                verbose_name="название (японское)")
+    image = models.ImageField(blank=True,
+                              verbose_name="изображение")
+    previous_evolution = models.ForeignKey("self",
+                                           on_delete=models.SET_NULL,
+                                           blank=True,
+                                           null=True,
+                                           verbose_name="предыдущая эволюция")
+    description = models.TextField(blank=True,
+                                   default="",
+                                   verbose_name="описание")
 
     def __str__(self) -> str:
         return self.title
 
+    class Meta:
+        verbose_name = "покемон"
+        verbose_name_plural = "покемоны"
+
 
 class PokemonEntity(models.Model):
-    latitude: models.FloatField = models.FloatField(verbose_name='Lat')
-    longitude: models.FloatField = models.FloatField(verbose_name='Lon')
-    pokemon: models.ForeignKey = models.ForeignKey(Pokemon,
-                                                   on_delete=models.CASCADE,
-                                                   verbose_name="pokemon")
-    appeared_at: models.DateTimeField = models.DateTimeField(blank=True,
-                                                             null=True)
-    disappeared_at: models.DateTimeField = models.DateTimeField(blank=True,
-                                                                null=True)
-    level: models.IntegerField = models.IntegerField(blank=True,
-                                                     null=True)
-    health: models.IntegerField = models.IntegerField(blank=True,
-                                                      null=True)
-    strength: models.IntegerField = models.IntegerField(blank=True,
-                                                        null=True)
-    defence: models.IntegerField = models.IntegerField(blank=True,
-                                                       null=True)
-    stamina: models.IntegerField = models.IntegerField(blank=True,
-                                                       null=True)
+    latitude = models.FloatField(verbose_name="широта")
+    longitude = models.FloatField(verbose_name="долгота")
+    pokemon = models.ForeignKey(Pokemon,
+                                on_delete=models.CASCADE,
+                                verbose_name="покемон")
+    appeared_at = models.DateTimeField(blank=True,
+                                       null=True,
+                                       verbose_name="появится с")
+    disappeared_at = models.DateTimeField(blank=True,
+                                          null=True,
+                                          verbose_name="исчезнет в")
+    level = models.IntegerField(blank=True,
+                                null=True,
+                                verbose_name="уровень")
+    health = models.IntegerField(blank=True,
+                                 null=True,
+                                 verbose_name="здоровье")
+    strength = models.IntegerField(blank=True,
+                                   null=True,
+                                   verbose_name="сила")
+    defence = models.IntegerField(blank=True,
+                                  null=True,
+                                  verbose_name="защита")
+    stamina = models.IntegerField(blank=True,
+                                  null=True,
+                                  verbose_name="выносливость")
 
     def __str__(self) -> str:
         return (f"{self.pokemon.title} {self.level}lvl "
                 f"{self.latitude,self.longitude}")
 
-    def is_active_in_timezone(self, timezone: timezone) -> bool:
+    def is_active_in_timezone(self, timezone: tzinfo) -> bool:
         if self.appeared_at and self.disappeared_at:
             return (self.appeared_at <
                     localtime(timezone=timezone) <
                     self.disappeared_at)
         return False
+
+    class Meta:
+        verbose_name = "объект покемона"
+        verbose_name_plural = "объекты покемонов"
